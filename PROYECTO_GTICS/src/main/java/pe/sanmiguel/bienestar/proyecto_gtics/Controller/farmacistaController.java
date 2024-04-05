cc75d513f9f561ffa8f720bb07b624444bfb6e63
blvck29 <a20210850@pucp.edu.pe>
2024-04-05 15:47:51 -0500

    feat: farmacista basic structure

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/farmacistaController.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/farmacistaController.java
@@ -1,4 +1,30 @@
 package pe.sanmiguel.bienestar.proyecto_gtics.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class farmacistaController {
+
+    @GetMapping("/farmacista")
+    public String farmacistaInicio() {
+        return "/farmacista/inicio";
+    }
+
+    @GetMapping("/farmacista/ordenes_venta")
+    public String OrdenesVenta() {
+        return "/farmacista/ordenes_venta";
+    }
+
+    @GetMapping("/farmacista/ordenes_web")
+    public String OrdenesWeb() {
+        return "/farmacista/ordenes_web";
+    }
+
+    @GetMapping("/farmacista/pre_ordenes")
+    public String preOrdenes() {
+        return "/farmacista/pre_ordenes";
+    }
+
 }
