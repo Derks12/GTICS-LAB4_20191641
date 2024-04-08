b6c13a0057e00fdf3c84b3f518b94b6b7da64550
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-04-08 00:33:14 -0500

    Colocar top-bar a las diferentes vistas
    Se coloco el modelo top-bar escogido por el grupo

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/adminSedeController.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/adminSedeController.java
@@ -2,13 +2,26 @@ package pe.sanmiguel.bienestar.proyecto_gtics.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = {"/adminsede"}, method = RequestMethod.GET)
 public class adminSedeController {
 
-    @GetMapping(value = {"/adminsede"})
+    @GetMapping(value = {""})
     public String showIndexAdminSede(){
         return "/adminSede/inicio";
+    }
+
+    @GetMapping(value = {"/doctores"})
+    public String showDoctors(){
+        return "/adminSede/doctores";
+    }
+
+    @GetMapping(value = {"/ordenes"})
+    public String showOrders(){
+        return "/adminSede/ordenes_reposicion";
     }
 
 }
