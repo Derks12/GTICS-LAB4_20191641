168b5c349097cc0daa29eb6d49366f8c4c325184
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-04-07 23:39:23 -0500

    flujo farmacista
    Se colocó el inicio del admin de sede

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/adminSedeController.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/adminSedeController.java
@@ -1,4 +1,14 @@
 package pe.sanmiguel.bienestar.proyecto_gtics.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class adminSedeController {
+
+    @GetMapping(value = {"/adminsede"})
+    public String showIndexAdminSede(){
+        return "/adminSede/inicio";
+    }
+
 }
