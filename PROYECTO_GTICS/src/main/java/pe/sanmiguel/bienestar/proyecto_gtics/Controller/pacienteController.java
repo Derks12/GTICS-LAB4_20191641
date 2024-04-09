dbecc929329fb992aaab26289ee89028b6947dbf
Pierohc <pieritohc@hotmail.com>
2024-04-09 11:10:02 -0500

    paciente/tracking

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/pacienteController.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/pacienteController.java
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class pacienteController {
 
     @GetMapping(value="")
-    public String preOrdenes(){
-        return "/paciente/pre_ordenes";}
+    public String preOrdenes(){return "/paciente/pre_ordenes";}
+
+
+    @GetMapping(value="/tracking")
+    public String tracking(){ return "/paciente/tracking";}
 
 
 
