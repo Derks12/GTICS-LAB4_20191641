5b7e5df6adfe1ad6533ea7227bf1f5bd7d70a358
Pierohc <pieritohc@hotmail.com>
2024-04-15 02:31:50 -0500

    paciente

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/pacienteController.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/pacienteController.java
@@ -1,10 +1,11 @@
 package pe.sanmiguel.bienestar.proyecto_gtics.Controller;
 
 
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.http.codec.multipart.Part;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping(value="/paciente", method= RequestMethod.GET)
@@ -27,6 +28,31 @@ public class pacienteController {
 
     @GetMapping(value = "/chatbot")
     public String chatbot(){return "/paciente/chatbot";}
+
+
+
+
+
+    @PostMapping(value = "/guardarOrden")
+    public String guardarOrden(@RequestParam(value = "name", required = false) String name ,
+                               @RequestParam(value = "lastname", required = false) String lastname,
+                               @RequestParam(value = "dni", required = false) String dni,
+                               @RequestParam(value = "edad", required = false) String edad,
+                               @RequestParam(value = "doctor", required = false) String doctor,
+                               @RequestParam(value = "fecha", required = false) String fecha,
+                               @RequestParam(value = "seguro", required = false) String seguro,
+                               @RequestParam(value = "correo", required = false) String correo,
+                               @RequestParam(value = "genero", required = false) String genero ){
+
+
+        System.out.println("Nombre:" + name);
+        System.out.println("Apellido: " + lastname);
+        System.out.println("Doctor: " + doctor);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("genero: " + genero);
+
+        return "redirect:/paciente/ordenes";
+    }
 
 
 }
