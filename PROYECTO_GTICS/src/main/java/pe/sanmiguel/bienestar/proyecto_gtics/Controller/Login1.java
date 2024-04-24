a7f7f93c88c97ffc720d2e0f24af5958eda4151a
Alexander Abrisqueta <a20191641@pucp.edu.pe>
2024-04-24 14:09:34 -0500

    una parte del registro

---
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Controller/Login1.java
@@ -0,0 +1,36 @@
+package pe.sanmiguel.bienestar.proyecto_gtics.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pe.sanmiguel.bienestar.proyecto_gtics.Entity.Usuario;
+import pe.sanmiguel.bienestar.proyecto_gtics.Repository.UsuarioRepository;
+
+//
+
+//@Controller
+//@RequestMapping("/login")
+//public class Login1 {
+//final UsuarioRepository usuarioRepository;
+
+//    public Login1(UsuarioRepository usuarioRepository) {this.usuarioRepository = usuarioRepository;}
+
+//    @GetMapping("/new")
+//   public String nuevoUsuario() {
+//       return "login/registro";
+//   }
+
+//  @PostMapping("/save")
+//   public String guardarNuevoUsuario(Usuario usuario, RedirectAttributes attributes){
+//       usuarioRepository.save(usuario);
+//      return "redirect:/index";
+//  }
+//
+
+
+
+//}
