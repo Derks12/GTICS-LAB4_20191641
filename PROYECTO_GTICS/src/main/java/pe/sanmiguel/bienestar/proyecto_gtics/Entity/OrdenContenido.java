968faddb612385ea013d0c877b1ca1462a74b626
Pierohc <pieritohc@hotmail.com>
2024-04-24 02:50:10 -0500

    aa

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Entity/OrdenContenido.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/Entity/OrdenContenido.java
@@ -10,11 +10,21 @@ import lombok.Setter;
 @Table(name = "orden_contenido")
 public class OrdenContenido {
 
+    @EmbeddedId
+    private OrdenContenidoId id;
 
-    @Column
-    private Integer idOrden;
-    @Column
-    private Integer idMedicamento;
+    @MapsId("idOrden")
+    @ManyToOne
+    @JoinColumn(name="idOrden")
+    private Orden idOrden;
+
+
+    @MapsId("idMedicamento")
+    @ManyToOne
+    @JoinColumn(name="idMedicamento")
+    private Medicamento idMedicamento;
+
+
     @Column
     private int cantidad;
 
