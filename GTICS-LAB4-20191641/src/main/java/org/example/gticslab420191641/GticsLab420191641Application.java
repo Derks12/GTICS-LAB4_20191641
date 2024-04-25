ec7c365ef826796d23fbb1b214aa646b66d50fb4
Alexander Abrisqueta <a20191641@pucp.edu.pe>
2024-04-24 19:14:34 -0500

    a

---
+++ GTICS-LAB4-20191641/src/main/java/org/example/gticslab420191641/GticsLab420191641Application.java
@@ -0,0 +1,13 @@
+package org.example.gticslab420191641;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab420191641Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab420191641Application.class, args);
+    }
+
+}
---
+++ GTICS-LAB4-20191641/src/test/java/org/example/gticslab420191641/GticsLab420191641ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gticslab420191641;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab420191641ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
